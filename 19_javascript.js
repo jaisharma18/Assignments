920ec8e52b56deba99af79827d90c88a446b9eaa
Jai Sharma <jai.sharma_cs18@gla.ac.in>
2020-12-05 18:16:06 +0530

    done

--- 19_javascript.js
+++ 19_javascript.js
@@ -13,14 +13,20 @@ var con = document.getElementById('content');
 n1.onclick = function() 
 {
     f1.style.left='-375px';
-    dp.style.width='250px';
-    con.style.height='303px';
+    dp.style.width='240px';
+    f2.style.right='0';
+    f2.style.left='0';
+    f3.style.right='-375px';
+    con.style.height='283px';
 }
 
 b1.onclick = function() 
 {
     dp.style.width='125px';
     f1.style.left='0';
+    f2.style.right='-375px';
+    f2.style.left='375px';
+    f3.style.right='-750px';
     con.style.height='283px';
 }
 
@@ -28,12 +34,16 @@ n2.onclick = function()
 {
     dp.style.width='375px';
     f1.style.left='-750px';
-    con.style.height='335px';
+    con.style.height='315px';
+    f2.style.left='-375px';
+    f3.style.right='0';
 }
 
 b2.onclick = function() 
 {
-    dp.style.width='250px';
+    dp.style.width='240px';
     f1.style.left='-375px';
-    con.style.height='303px';
+    con.style.height='283px';
+    f2.style.left='0';
+    f3.style.right='-375px';
 }
